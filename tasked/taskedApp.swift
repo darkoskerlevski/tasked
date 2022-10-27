91dea4e7937b32d3be2ea9aca654a42814b94fb2
Darko Skerlevski <darko.skerlevski@netcetera.com>
2022-10-27 13:09:07 +0200

    Initial Commit

---
+++ tasked/taskedApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskedApp.swift
+//  tasked
+//
+//  Created by Darko Skerlevski on 27.10.22.
+//
+
+import SwiftUI
+
+@main
+struct taskedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
