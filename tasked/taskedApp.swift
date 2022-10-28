7bc420300b24a7cdb120a715900dce93659b465e
Darko Skerlevski <darko.skerlevski@netcetera.com>
2022-10-28 11:40:24 +0200

    initial commit pt2

--- tasked/taskedApp.swift
+++ tasked/taskedApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct taskedApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: tasksViewModel())
         }
     }
 }
---
+++ tasked/tasksViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  tasksViewModel.swift
+//  tasked
+//
+//  Created by Darko Skerlevski on 27.10.22.
+//
+
+import Foundation
+import SwiftUI
+
+class tasksViewModel: ObservableObject {
+//    @Published var tasks: [tasksViewModel] = []
+    
+    
+
+    
+}
